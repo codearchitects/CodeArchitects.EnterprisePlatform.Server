06a581cdacbf65e7fc708edebec2daf8f33734a2
gdifilippo <gdifilippo@codearchitects.com>
2021-09-07 09:38:44 +0200

    Nuget pipeline test

---
+++ src/Infrastructure.Dapr.AspNetCore/ClassForNugetPipelineTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeArchitects.Platform.Infrastructure.Dapr.AspNetCore
+{
+  /// <summary>
+  /// Xml doc test.
+  /// </summary>
+  public static class ClassForNugetPipelineTest
+  {
+    /// <summary>
+    /// Xml doc test.
+    /// </summary>
+    public static void HelloWorld() => Console.WriteLine("Hello, Nuget!");
+  }
+}
