28de3d9fa216d8b1cf30c2683228794dd7ef06d3
gdifilippo <gdifilippo@codearchitects.com>
2021-10-05 14:50:34 +0200

    Added analyzer

---
+++ tests/Analyzer.Tests/AnalyzerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CodeArchitects.Platform.Analyzer.Tests
+{
+  public abstract class AnalyzerTest : CompilationTest
+  {
+    protected Task<ImmutableArray<Diagnostic>> GetDiagnosticsAsync(string code)
+      => GetCompilationDataAsync(code)
+        .ContinueWith(x => x.Result.Diagnostics);
+  }
+}
