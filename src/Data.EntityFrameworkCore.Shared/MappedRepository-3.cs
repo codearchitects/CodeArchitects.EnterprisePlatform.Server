707245c4cef8093864385f86b5a8f897b0bc63b2
gdifilippo <gdifilippo@codearchitects.com>
2022-12-01 10:04:33 +0100

    Add mapped repository

---
+++ src/Data.EntityFrameworkCore.Shared/MappedRepository-3.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeArchitects.Platform.Data.EntityFrameworkCore;
+
+internal abstract class MappedRepository<TTable, TEntity, TKey> : IRepository<TEntity, TKey>
+  where TTable : class
+  where TEntity : class
+  where TKey : IEquatable<TKey>
+{
+  private readonly IDataContext _context;
+
+  public MappedRepository(IDataContext context)
+  {
+    _context = context;
+  }
+
+  protected DbContext DbContext => _context.DbContext;
+
+  protected DbSet<TTable> Entities => _context.DbContext.Set<TTable>();
+
+  protected abstract TTable EntityToTable(TEntity entity);
+
+  protected abstract TEntity TableToEntity(TTable table);
+
+  public virtual async Task<TEntity?> FindAsync(TKey key, CancellationToken cancellationToken = default)
+  {
+    TTable? table = await _context.FindAsync<TTable, TKey>(key, cancellationToken);
+    
+    return table is not null
+      ? TableToEntity(table)
+      : null;
+  }
+
+  public virtual Task<TEntity?> FindAsync(TKey key, IncludeAction<TEntity> includeAction, CancellationToken cancellationToken = default)
+  {
+    throw new NotSupportedException();
+  }
+
+  public virtual Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default)
+  {
+    TTable table = EntityToTable(entity);
+    return _context.InsertAsync<TTable, TKey>(table, cancellationToken);
+  }
+
+  public virtual Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+  {
+    TTable table = EntityToTable(entity);
+    return _context.UpdateAsync<TTable, TKey>(table, cancellationToken);
+  }
+
+  public virtual Task RemoveAsync(TKey key, CancellationToken cancellationToken = default)
+  {
+    return _context.RemoveAsync<TTable, TKey>(key, cancellationToken);
+  }
+}
---
+++ src/Data.EntityFrameworkCore.Shared/MappedRepository-4.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeArchitects.Platform.Data.EntityFrameworkCore;
+
+internal abstract class MappedRepository<TDbContext, TTable, TEntity, TKey> : MappedRepository<TTable, TEntity, TKey>
+  where TDbContext : DbContext
+  where TTable : class
+  where TEntity : class
+  where TKey : IEquatable<TKey>
+{
+  public MappedRepository(IDataContext<TDbContext> context)
+    : base(context)
+  {
+    DbContext = context.DbContext;
+  }
+
+  protected new TDbContext DbContext { get; }
+}
