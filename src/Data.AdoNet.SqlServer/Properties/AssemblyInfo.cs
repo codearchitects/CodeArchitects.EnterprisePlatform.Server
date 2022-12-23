52beabc48d164e71e8307876bb59e0470de68819
gdifilippo <gdifilippo@codearchitects.com>
2022-12-23 12:28:06 +0100

    Temporarely remove SQLServer folder

--- src/Data.AdoNet.SqlServer/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("CodeArchitects.Platform.Data.AdoNet.FunctionalTests")]
-[assembly: InternalsVisibleTo("CodeArchitects.Platform.Data.AdoNet.UnitTests")]
-[assembly: InternalsVisibleTo("CodeArchitects.Platform.Data.IntegrationTests")]
