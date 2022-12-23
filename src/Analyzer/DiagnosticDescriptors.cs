e337fd157551e8e8446490597ebb66a879433cbd
gdifilippo <gdifilippo@codearchitects.com>
2022-12-23 15:59:29 +0100

    Chore

--- src/Analyzer/DiagnosticDescriptors.cs
+++ src/Analyzer/DiagnosticDescriptors.cs
@@ -4,7 +4,7 @@ namespace CodeArchitects.Platform.Analyzer;
 
 internal static class DiagnosticDescriptors
 {
-  public static readonly DiagnosticDescriptor CAESP001 = new(
+  public static readonly DiagnosticDescriptor CAESP001 = new DiagnosticDescriptor(
     DiagnosticIds.CAESP001,
     "Experimental feature",
     "This is an experimental feature. It may be changed or removed in future versions.",
@@ -12,7 +12,7 @@ internal static class DiagnosticDescriptors
     DiagnosticSeverity.Warning,
     isEnabledByDefault: true);
 
-  public static readonly DiagnosticDescriptor CAESP002 = new(
+  public static readonly DiagnosticDescriptor CAESP002 = new DiagnosticDescriptor(
     DiagnosticIds.CAESP002,
     "Queryable leak",
     "Exposing IQueryable from a repository interface is not recommended. Consider declaring a specific method for each query.",
