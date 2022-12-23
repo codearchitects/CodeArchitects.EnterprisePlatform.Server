883e0dba2b5e8d9dab7b46cb40ed108246f16baf
gdifilippo <gdifilippo@codearchitects.com>
2022-12-23 12:28:34 +0100

    Re-add SQLServer

---
+++ src/Data.AdoNet.SQLServer/SQLServerProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CodeArchitects.Platform.Data.AdoNet.SQLServer;
+
+/// <summary>
+/// Extension methods for <see cref="SQLServerProvider"/>.
+/// </summary>
+public static class SQLServerProviderExtensions
+{
+  /// <summary>
+  /// Specifies a factory that creates instances of <see cref="SqlConnection"/>.
+  /// </summary>
+  /// <typeparam name="TConnectionFactory">The connection factory type.</typeparam>
+  /// <param name="provider">The Oracle provider.</param>
+  /// <returns>The same <see cref="SQLServerProvider"/> for further configuration.</returns>
+  public static SQLServerProvider UseConnectionFactory<TConnectionFactory>(this SQLServerProvider provider)
+    where TConnectionFactory : IConnectionFactory<SqlConnection>
+  {
+    return provider.UseConnectionFactory(typeof(TConnectionFactory));
+  }
+}
