8020e5d81be898432279a729dd550ee25ceef8c6
gdifilippo <gdifilippo@codearchitects.com>
2023-03-03 17:32:45 +0100

    Add missing docs

--- src/Actors/ActorIdTypeAttribute-1.cs
+++ src/Actors/ActorIdTypeAttribute-1.cs
@@ -2,8 +2,15 @@
 
 namespace CodeArchitects.Platform.Actors;
 
+/// <summary>
+/// Specifies the type to use for the actor id.
+/// </summary>
+/// <typeparam name="TActorId">The actor id type.</typeparam>
 [AttributeUsage(AttributeTargets.Class)]
 public class ActorIdTypeAttribute<TActorId> : Attribute, IActorIdTypeAttribute
 {
+  /// <summary>
+  /// The actor id type.
+  /// </summary>
   public Type IdType => typeof(TActorId);
 }
--- src/Actors/ActorIdTypeAttribute.cs
+++ src/Actors/ActorIdTypeAttribute.cs
@@ -2,13 +2,23 @@
 
 namespace CodeArchitects.Platform.Actors;
 
+/// <summary>
+/// Specifies the type to use for the actor id.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class ActorIdTypeAttribute : Attribute, IActorIdTypeAttribute
 {
+  /// <summary>
+  /// Creates a new instance of <see cref="ActorIdTypeAttribute"/>.
+  /// </summary>
+  /// <param name="idType">The actor id type.</param>
   public ActorIdTypeAttribute(Type idType)
   {
     IdType = idType;
   }
 
+  /// <summary>
+  /// The actor id type.
+  /// </summary>
   public Type IdType { get; }
 }
