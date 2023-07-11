2cde5cdf8432c63335d7d787a105cfba8d1b6eda
gdifilippo <gdifilippo@codearchitects.com>
2023-07-11 10:36:41 +0200

    Fix multitenancy seeding issue

---
+++ src/Data.EntityFrameworkCore/Features/Multitenancy/MultitenancyContextWrapper.cs
@@ -0,0 +1,39 @@
+﻿using CodeArchitects.Platform.Data.Features.Multitenancy;
+
+namespace CodeArchitects.Platform.Data.EntityFrameworkCore.Features.Multitenancy;
+
+internal class MultitenancyContextWrapper : IMultitenancyContext, IMultitenancyContextBypasser
+{
+  private readonly IMultitenancyContext _context;
+  private bool _isBypassed;
+
+  public MultitenancyContextWrapper(IMultitenancyContext context)
+  {
+    _context = context;
+  }
+
+  public bool ShouldFilter => !_isBypassed && _context.ShouldFilter;
+
+  public object TenantId => _context.TenantId;
+
+  public IDisposable BypassMultitenancy()
+  {
+    return new BypassScope(this);
+  }
+
+  private sealed class BypassScope : IDisposable
+  {
+    private readonly MultitenancyContextWrapper _context;
+
+    public BypassScope(MultitenancyContextWrapper context)
+    {
+      _context = context;
+      _context._isBypassed = true;
+    }
+
+    public void Dispose()
+    {
+      _context._isBypassed = false;
+    }
+  }
+}
