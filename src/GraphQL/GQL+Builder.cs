84707ba433b72f9a0532583c3bccf0c78159b197
gdifilippo <gdifilippo@codearchitects.com>
2023-07-12 13:32:10 +0200

    Add GQL Builder

---
+++ src/GraphQL/GQL+Builder.cs
@@ -0,0 +1,80 @@
+﻿using CodeArchitects.Platform.GraphQL.Document;
+using CodeArchitects.Platform.GraphQL.Document.Builder;
+
+namespace CodeArchitects.Platform.GraphQL;
+
+public static partial class GQL
+{
+  public abstract class Builder : Builder<IDocumentRoot>
+  {
+    private static Type s_compilerType = typeof(DefaultCompiler<>);
+
+    private Builder() { }
+
+    internal static void SetCompilerType(Type compilerType)
+    {
+      bool isValid =
+        compilerType.IsGenericType &&
+        compilerType.BaseType.IsGenericType &&
+        compilerType.BaseType.GetGenericTypeDefinition() == typeof(Builder<>) &&
+        compilerType.GetConstructor(Type.EmptyTypes) is not null;
+
+      if (!isValid)
+        throw new ArgumentException($"'{nameof(compilerType)}' should be a generic concrete type extending '{nameof(Builder<object>)}'<> with a parameterless constructor.");
+
+      s_compilerType = compilerType;
+    }
+
+    internal static Builder<TDocumentRoot> CreateInstance<TDocumentRoot>()
+      where TDocumentRoot : class
+    {
+      return (Builder<TDocumentRoot>)Activator.CreateInstance(s_compilerType.MakeGenericType(typeof(TDocumentRoot)));
+    }
+  }
+
+  public abstract class Builder<TDocumentRoot>
+    where TDocumentRoot : class
+  {
+    private static readonly Lazy<Builder<TDocumentRoot>> s_lazyInstance = new(Builder.CreateInstance<TDocumentRoot>);
+
+    private protected Builder() { }
+
+    private protected abstract IGraphDocument<TResult> CompileDocument<TResult>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult>> buildDocument)
+      where TResult : class;
+
+    private protected abstract IGraphDocument<TResult, TVariables> CompileDocument<TResult, TVariables>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult, TVariables>> buildDocument)
+      where TResult : class
+      where TVariables : class;
+
+    public static IGraphDocument<TResult> Compile<TResult>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult>> buildDocument)
+      where TResult : class
+    {
+      return s_lazyInstance.Value.CompileDocument(buildDocument);
+    }
+
+    public static IGraphDocument<TResult, TVariables> Compile<TResult, TVariables>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult, TVariables>> buildDocument)
+      where TResult : class
+      where TVariables : class
+    {
+      return s_lazyInstance.Value.CompileDocument(buildDocument);
+    }
+  }
+
+  private sealed class DefaultCompiler<TDocumentRoot> : Builder<TDocumentRoot>
+    where TDocumentRoot : class
+  {
+    private protected override IGraphDocument<TResult> CompileDocument<TResult>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult>> buildDocument)
+      => throw NotImplemented();
+
+    private protected override IGraphDocument<TResult, TVariables> CompileDocument<TResult, TVariables>(Func<IDocumentBuilder<TDocumentRoot>, IGraphDocument<TResult, TVariables>> buildDocument)
+      => throw NotImplemented();
+
+    private static NotImplementedException NotImplemented() => new NotImplementedException("The current CodeArchitects.Platform.GraphQL implementation does not support document compilation.");
+  }
+}
+
+public static class C
+{
+  public static readonly IGraphDocument<object> Doc = GQL.Builder.Compile(x => x.Query(_ => _
+    .ExpandRef())
+}
--- src/GraphQL/GQL+Query.cs
+++ src/GraphQL/GQL+Query.cs
@@ -3,55 +3,58 @@ using System.Linq.Expressions;
 
 namespace CodeArchitects.Platform.GraphQL;
 
-public static class GQL
+public static partial class GQL
 {
   public static TResult ExpandRef<TSource, TResult>(TSource? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
     where TSource : notnull
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IEnumerable<TResult> ExpandCol<TSource, TResult>(IEnumerable<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IReadOnlyCollection<TResult> ExpandCol<TSource, TResult>(IReadOnlyCollection<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static ICollection<TResult> ExpandCol<TSource, TResult>(ICollection<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IReadOnlyList<TResult> ExpandCol<TSource, TResult>(IReadOnlyList<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IList<TResult> ExpandCol<TSource, TResult>(IList<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static List<TResult> ExpandCol<TSource, TResult>(List<TSource>? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static TResult[] ExpandCol<TSource, TResult>(TSource[]? source, Expression<Func<IFieldBuilder<TSource>, IBuildResult<TResult>>> expansion)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
 
   public static TResult SelectRef<TSource, TResult>(TSource? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IEnumerable<TResult> SelectCol<TSource, TResult>(IEnumerable<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IReadOnlyCollection<TResult> SelectCol<TSource, TResult>(IReadOnlyCollection<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static ICollection<TResult> SelectCol<TSource, TResult>(ICollection<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IReadOnlyList<TResult> SelectCol<TSource, TResult>(IReadOnlyList<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static IList<TResult> SelectCol<TSource, TResult>(IList<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static List<TResult> SelectCol<TSource, TResult>(List<TSource>? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
 
   public static TResult[] SelectCol<TSource, TResult>(TSource[]? source, Expression<Func<TSource, TResult>> selection)
-    => throw new NotImplementedException("This method is to be used in document building expressions only.");
+    => throw NotImplemented();
+
+
+  private static NotImplementedException NotImplemented() => new NotImplementedException("This method is to be used in document building expressions only.");
 }
