cd0e712c8b5805c00dfbb8936c4aa503c1be7c2e
Gianvito Difilippo <gdifilippo@codearchitects.com>
2023-10-19 16:23:23 +0200

    Richiesta pull 3292 sottoposta a merge: Test
    .

--- tests/GraphQL.ChilliCream.UnitTests/RequestFactoryTests.cs
+++ tests/GraphQL.ChilliCream.UnitTests/RequestFactoryTests.cs
@@ -13,6 +13,7 @@ public class RequestFactoryTests
 
   public RequestFactoryTests()
   {
+    Console.WriteLine("Hello, world"); // TODO: Delete
     _executorProviderMock = new(MockBehavior.Strict);
     _executorProviderMock
       .Setup(x => x.GetExecutor<object>())
